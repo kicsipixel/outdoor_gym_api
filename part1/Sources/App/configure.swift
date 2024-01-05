0025b5a0c8247c8b9036c93f46ce46a488f05be9
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-01-05 21:44:33 +0100

    New: / route was added.

--- part1/Sources/App/configure.swift
+++ part1/Sources/App/configure.swift
@@ -7,20 +7,20 @@ public func configure(_ app: Application) async throws {
     
     // MARK: - Local PostgreSQL database
     /*
-     //  Get values from .env.development
-     guard let username = Environment.get("POSTGRES_USER") else { return }
-     guard let password = Environment.get("POSTGRES_PASSWORD") else { return }
-     guard let database = Environment.get("POSTGRES_DB") else { return }
-     
-     //   Configure DB connection
-     app.databases.use(.postgres(configuration: SQLPostgresConfiguration(hostname: "localhost",
-     username: username,
-     password: password,
-     database: database,
-     tls: PostgresConnection.Configuration.TLS.disable)),
-     as: .psql)
-     */
+    //  Get values from .env.development
+    guard let username = Environment.get("POSTGRES_USER") else { return }
+    guard let password = Environment.get("POSTGRES_PASSWORD") else { return }
+    guard let database = Environment.get("POSTGRES_DB") else { return }
     
+    //   Configure DB connection
+    app.databases.use(.postgres(configuration: SQLPostgresConfiguration(hostname: "localhost",
+                                                                        username: username,
+                                                                        password: password,
+                                                                        database: database,
+                                                                        tls: PostgresConnection.Configuration.TLS.disable)),
+                      as: .psql)
+    
+   */
     // MARK: - Remote PostgreSQL database
     guard let hostname = Environment.get("REMOTE_POSTGRES_DB") else { return }
     guard let username = Environment.get("REMOTE_POSTGRES_USER") else { return }
@@ -33,7 +33,7 @@ public func configure(_ app: Application) async throws {
                                                                         database: username,
                                                                         tls: PostgresConnection.Configuration.TLS.disable)),
                       as: .psql)
-    
+
     // Migrations
     app.migrations.add(CreateGymTableMigration())
     app.migrations.add(AddCityAndCountryToGymTableMigration())
--- part1/Sources/App/routes.swift
+++ part1/Sources/App/routes.swift
@@ -1,5 +1,9 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    app.get { req in
+        return "It works!🚀"
+    }
+    
     try app.register(collection: GymController())
 }
