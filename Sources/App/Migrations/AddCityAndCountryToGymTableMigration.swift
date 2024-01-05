d81645c65fb94aada94bb89f864504cef86603b1
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-01-05 19:57:45 +0100

    Fixed: Folder structure.

--- Sources/App/Migrations/AddCityAndCountryToGymTableMigration.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Szabolcs Tóth on 16.12.2023.
-//  Copyright © 2023 Szabolcs Tóth. All rights reserved.
-//
-
-import Foundation
--- Sources/App/Models/Gym.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Szabolcs Toth on 11.12.2023.
-//
-
-import Foundation
