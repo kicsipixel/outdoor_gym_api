176f267f930666aac0bcda64ccb0b54445b7a2a0
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-01-06 22:30:59 +0100

    New: CreateuserTableMigration was added.

---
+++ part2/Sources/App/Migrations/CreateUserTableMigration.swift
@@ -0,0 +1,25 @@
+//
+//  CreateUserTableMigration.swift
+//  
+//
+//  Created by Szabolcs Tóth on 06.01.2024.
+//  Copyright © 2024 Szabolcs Tóth. All rights reserved.
+//
+
+import Fluent
+
+struct CreateUserTableMigration: AsyncMigration {
+    func prepare(on database: FluentKit.Database) async throws {
+        try await database.schema("users")
+            .id()
+            .field("username", .string, .required)
+            .field("password", .string, .required)
+            .unique(on: "username")
+            .create()
+    }
+    
+    func revert(on database: FluentKit.Database) async throws {
+        try await database.schema("users")
+            .delete()
+    }
+}
