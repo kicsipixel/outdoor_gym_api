f98e39f379dd93d376bc6d7108da73e91cbecfe7
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-02-23 21:51:48 +0100

    Fixed: Database configuration was fixed.

--- part2/Sources/App/configure.swift
+++ part2/Sources/App/configure.swift
@@ -6,34 +6,29 @@ import Vapor
 public func configure(_ app: Application) async throws {
     
     // MARK: - Local PostgreSQL database
-    /*
-    //  Get values from .env.development
-    guard let username = Environment.get("POSTGRES_USER") else { return }
-    guard let password = Environment.get("POSTGRES_PASSWORD") else { return }
-    guard let database = Environment.get("POSTGRES_DB") else { return }
-    
     //   Configure DB connection
-    app.databases.use(.postgres(configuration: SQLPostgresConfiguration(hostname: "localhost",
-                                                                        username: username,
-                                                                        password: password,
-                                                                        database: database,
-                                                                        tls: PostgresConnection.Configuration.TLS.disable)),
-                      as: .psql)
+    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        tls: .prefer(try .init(configuration: .clientDefault)))
+    ), as: .psql)
     
-   */
     // MARK: - Remote PostgreSQL database
-    guard let hostname = Environment.get("REMOTE_POSTGRES_DB") else { return }
-    guard let username = Environment.get("REMOTE_POSTGRES_USER") else { return }
-    guard let password = Environment.get("REMOTE_POSTGRES_PASSWORD") else { return }
+    /*
+     // Configure DB connection
+     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+     hostname: Environment.get("REMOTE_POSTGRES_DB") ?? "localhost",
+     port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+     username: Environment.get("REMOTE_POSTGRES_USER") ?? "vapor_username",
+     password: Environment.get("REMOTE_POSTGRES_PASSWORD") ?? "vapor_password",
+     database: Environment.get("REMOTE_POSTGRES_USER") ?? "vapor_database",
+     tls: .prefer(try .init(configuration: .clientDefault)))
+     ), as: .psql)
+     */
     
-    // Configure DB connection
-    app.databases.use(.postgres(configuration: SQLPostgresConfiguration(hostname: hostname,
-                                                                        username: username,
-                                                                        password: password,
-                                                                        database: username,
-                                                                        tls: PostgresConnection.Configuration.TLS.disable)),
-                      as: .psql)
-
     // Migrations
     app.migrations.add(CreateUserTableMigration())
     app.migrations.add(CreateGymTableMigration())
